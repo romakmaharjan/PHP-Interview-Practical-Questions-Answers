25690e0f8d240e366225bf27d50627104fd585d4
romak maharjan <romakmaharjan@gmail.com>
2023-10-17 16:33:50 +0545

    Basic pattern added

---
+++ BasicPattern/pattern.php
@@ -0,0 +1,22 @@
+<?php
+/*Basic pattern concept */
+for($i=1; $i<=5; $i++){
+    for($j=0; $j<$i; $j++){
+        echo "*";
+    }
+
+echo "<br/>";
+}
+
+?>
+<?php
+/*Basic pattern concept */
+for($i=1; $i<=5; $i++){
+    for($j=0; $j<$i; $j++){
+        echo "$i";
+    }
+
+echo "<br/>";
+}
+
+?>
